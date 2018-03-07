f3ba34a4ce992c6be76285f716d67ec67a83f47b
Unknown <218300@student.pwr.edu.pl>
2018-03-07 15:25:15 +0100

    class for mcts

---
+++ hs_projekt_wzsi/MCTS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hs_projekt_wzsi
+{
+    class MCTS
+    {
+        public void Selection()
+        {
+            
+        }
+    }
+}
